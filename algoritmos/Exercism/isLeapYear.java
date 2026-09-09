e3e979c6292bd114b22ea46636470314e141723c
PEDRO HENRIQUE NUNES OLIVEIRA <160438856+PedroHenri10@users.noreply.github.com>
2026-09-09 14:38:04 -0300

    Implement isLeapYear method in Leap class

---
+++ algoritmos/Exercism/isLeapYear.java
@@ -0,0 +1,13 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)){
+            return true;
+        }else{
+            return false;
+        }
+
+        //  return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+    }
+
+}
