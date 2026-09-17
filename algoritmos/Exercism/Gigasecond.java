f25072d9cc48b2fbf8a0a3e66b903139668e833a
PEDRO HENRIQUE NUNES OLIVEIRA <160438856+PedroHenri10@users.noreply.github.com>
2026-09-17 16:37:40 -0300

    Add Gigasecond class to calculate future date

---
+++ algoritmos/Exercism/Gigasecond.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Gigasecond {
+    LocalDateTime moment;
+    public Gigasecond(LocalDate moment) {
+        this.moment = moment.atStartOfDay();
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+        this.moment = moment;
+    }
+
+    public LocalDateTime getDateTime() {
+        return moment.plusSeconds(1000000000);
+    }
+}
