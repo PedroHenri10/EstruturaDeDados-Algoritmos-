2afb5577af102c279f144c2a28d604d548ef5ee5
PEDRO HENRIQUE NUNES OLIVEIRA <160438856+PedroHenri10@users.noreply.github.com>
2026-09-17 11:19:28 -0300

    Add ResistorColor class with color coding methods

---
+++ algoritmos/Exercism/ResistorColor.java
@@ -0,0 +1,16 @@
+class ResistorColor {
+    final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    int colorCode(String color) {
+        for(int i = 0; i < colors.length; i++)
+            if(color.equals(colors[i])) {
+                return i;
+            }
+        return -1;
+    }
+
+    String[] colors() {
+
+        return colors;
+    }
+}
